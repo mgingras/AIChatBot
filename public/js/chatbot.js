27cc767efdd9cf332899d940415741aeeae59dee
Martin Gingras <martin@mgingras.ca>
2015-04-01 15:23:14 -0400

    cleaned up styling and some shit

--- public/js/chatbot.js
+++ public/js/chatbot.js
@@ -3,8 +3,8 @@ function ChatBot(){
 }
 
 ChatBot.prototype.say = function(message) {
-  var msg = '<div class="col-xs-10 pull-right"><img src="/img/chatbot.png" alt="" class="avatar"><h4>ChatBot&nbsp;'+
-            '<span class="small">'+ (new Date()).toLocaleTimeString().replace(/:[0-9]{2} /, ' ') +'</span></h4>' +
-            '<p>'+ message +'</p></div>';
-  $('.message').append(msg);
+  var msg = '<div class="col-xs-10"><img src="/img/chatbot.png" alt="C" class="userAvatar pull-left" style="float:left"><h4 class="pull-left" style="float:left;">ChatBot&nbsp;'+
+            '<div class="small">'+ (new Date()).toLocaleTimeString().replace(/:[0-9]{2} /, ' ') +'</div></h4>' +
+            '<p class="pull-left" style="margin-left:20px;margin-top:20px;">'+ message +'</p></div>';
+  $('.chat').append(msg);
 };
--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,41 @@
 /* globals ChatBot */
 var chatbot;
-var user = null;
+var user = {};
 $(function() {
-  while(user === null || user === ""){
-    user = prompt("Please enter your name");
+  var username = null;
+  while(username === null || username === ""){
+    username = prompt("Please enter your name");
   }
+  user.username = username;
   chatbot = new ChatBot();
+  $('#send').click(sendMessage);
+  $('#message').keypress(function(e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      sendMessage();
+    };
+  });
 });
+
+function sendMessage() {
+  var msg = $('#message').val();
+  $('#message').val('');
+  say(msg);
+  chatbot.say(msg);
+  $('#chat').scrollTop($('#chat')[0].scrollHeight);
+}
+
+function say(text) {
+  var msg = ''+
+    '<div class="row message">'+
+      '<div class="col-xs-10 col-xs-offset-2"><i class="userAvatar avatar avatar-color-33 avatar-plain pull-right avatar-letter-'+user.username[0].toLowerCase()+'"></i>'+
+        '<h4 class="pull-right">'+ user.username +
+          '<div class="small">' + (new Date()).toLocaleTimeString().replace(/:[0-9]{2} /, ' ') + '</div>'+
+        '</h4>'+
+        '<p class="pull-right" style="margin-right:20px;margin-top:20px;">'+ text +
+        '</p>' +
+      '</div>'+
+    '</div>';
+  $('.chat').append(msg);
+}
+
